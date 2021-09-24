6d28c662411babbe60708dee86f39bd6177982b2
MagistrLoiso <dmtred.h@gmail.com>
2021-09-24 22:15:41 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,8 +1,15 @@
-# -*- coding: utf-8 -*
-
+#Task 1
 def is_even(x):
     if x<0:
         print("You've typed negative number!")
     return bool(not x%2)
 
-print(is_even(-2))
+#Task2
+def generate_squares(min_num, max_num):
+    l = []
+    for i in range(min_num, max_num+1):
+        l.append(i**2)
+    return l
+
+print (generate_squares(1, 5))
+
